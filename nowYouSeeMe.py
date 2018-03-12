fee812295312bad40e4b2f6ec964eaaf4a2a9a5e
Unknown <giorgos.livas.99@gmail.com>
2018-03-12 02:02:16 +0000

    initial commit

---
+++ nowYouSeeMe.py
@@ -0,0 +1,10 @@
+from urllib.request import urlopen
+
+def internet_on():
+    try:
+        page = urlopen('http://216.58.192.142', timeout=1)
+        return True
+    except page.URLError as err:
+        return False
+
+    
