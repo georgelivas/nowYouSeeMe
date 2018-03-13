a16066b6f11f923f8fe7b2974f8e4bc272fb9747
George Livas <giorgos.livas.99@gmail.com>
2018-03-13 01:51:29 +0000

    Update nysm.sh

--- nysm.sh
+++ nysm.sh
@@ -6,5 +6,5 @@ if [[ $version =~ .*3..* ]]
 then
     python3 nowYouSeeMe.py
 else
-    echo "You need python 3.1, or later to run this game."
+    echo "You need python 3.1, or later to run this tool."
 fi
